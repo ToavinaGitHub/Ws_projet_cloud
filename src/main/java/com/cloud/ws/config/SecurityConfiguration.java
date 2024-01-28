d539c0d338afe1d072f6e9339613f7a68448269b
Zak <118508607+ToavinaGitHub@users.noreply.github.com>
2024-01-28 13:49:41 +0300

    Beta version of WS (2)

--- src/main/java/com/cloud/ws/config/SecurityConfiguration.java
+++ src/main/java/com/cloud/ws/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
                 .and()
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/register","/api/v1/auth/authenticate","/Annonces","/AnnoncesDispo")
+                .requestMatchers("/api/v1/auth/register","/api/v1/auth/authenticate","/Annonces","/AnnoncesDispo","/Annonce")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
